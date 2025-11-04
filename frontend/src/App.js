17c8950ae91b7ca756cbbff7da5dc0a52dc8ec4c
Rex White <rexwhite@gmail.com>
2025-11-03 21:03:14 -0500

    Moved front-end hosting to server.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
+    fetch('/api/hello')
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error('Error fetching from backend:', err));
